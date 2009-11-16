77b48cf2f1a30a529a7b7c74446098f28f2bda14
Dave Vieglais <dave.vieglais@gmail.com>
2009-11-16 17:06:22 +0000

    updating

--- source/conf.py
+++ source/conf.py
@@ -193,7 +193,7 @@ latex_documents = [
 latex_use_parts = False
 
 # Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
+latex_preamble = '\\usepackage{pdflscape}'
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
